cac3e3d6b5516af4c3a06abbcb4080eef296ad58
Andreas Thomas <dev@chronark.com>
2021-03-31 11:20:40 +0200

    feat: wip

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,12 +1,23 @@
 module.exports = {
-    extends: [
-        'airbnb',
-        'eslint:recommended',
-    ],
-
-
-    // Comment to trigger git diff
-    plugins: [],
-    rules: {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json'
     },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+        'airbnb-typescript'
+    ],
+    rules:{
+      "@typescript-eslint/indent": ["error", 4],
+      "react/react-in-jsx-scope": "off",
+      "import/prefer-default-export": "off",
+      "jsx-a11y/anchor-is-valid": "off",
+      "react/jsx-indent": ["error", 4],
+      "react/jsx-indent-props": ["error", 4],
+      "object-curly-newline": "off",
+      "max-len": [2, {"code": 155, "ignoreUrls": true }]
+    }
 };
