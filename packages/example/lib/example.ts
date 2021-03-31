6ec3ef4db5cfe398c1333c670f04510e514ebf37
Andreas Thomas <dev@chronark.com>
2021-03-31 11:37:18 +0200

    feat: add typescript default

--- packages/example/lib/example.ts
+++ packages/example/lib/example.ts
@@ -1,3 +1,6 @@
+
+// The default @trieb-work/tsconfig-base would not allow unused parameters
+// but we have overwritten that in the local tsconfig.json and typechecking passes.
 function HelloWorld(greeting: string): string {
-    return a;
+    return "hello World";
 }
