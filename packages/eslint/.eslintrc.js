f222dafae68885244a4d7caabf7367ef0adac223
Andreas Thomas <dev@chronark.com>
2021-04-06 12:55:10 +0200

    feat: disable react/prop-types and react/jsx-props-no-spreading
    Also add some documentation for some rules

--- packages/eslint/.eslintrc.js
+++ packages/eslint/.eslintrc.js
@@ -9,11 +9,24 @@ module.exports = {
   rules: {
     "@typescript-eslint/indent": ["error", 4],
     "react/react-in-jsx-scope": "off",
+
+    /*
+     * Named exports are almost always better because they break when the imported module changes
+     */
     "import/prefer-default-export": "off",
+
     "jsx-a11y/anchor-is-valid": "off",
-    "react/jsx-indent": ["error", 4],
-    "react/jsx-indent-props": ["error", 4],
     "object-curly-newline": "off",
     "max-len": [2, { code: 154, ignoreUrls: true }],
+
+    /*
+     * We are using typescript and don't need prop-types
+     */
+    "react/prop-types": "off",
+
+    /*
+     * Nextjs does this anyways and it's really handy for component controller logic
+     */
+    "react/jsx-props-no-spreading": "off",
   },
 };
